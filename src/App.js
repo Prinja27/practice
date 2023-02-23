9a7e2e6f3dc8497f034ba5ffe0eea66afd2b9e5c
Prinja27 <aayushprinja763@gmail.com>
2023-02-23 21:11:47 +0530

    new practice coder-dost

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'Aayush'
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='App-header'>
+        {name} Prinja
+        <Demo />
+      </div>
     </div>
   );
+}
+
+function Demo() {
+  const newName = "Sakshi"
+  return (
+    <div className='App' >
+      <div className='App-header'>
+        {newName} Prinja
+      </div>
+    </div>
+  )
 }
 
 export default App;
